42fabe1eff09433e22e629e27585745d45e1f6b5
FRANCIELE PASTORI <103603841+FranPastori@users.noreply.github.com>
2023-01-21 17:47:34 -0300

    Testando

--- Js/main.js
+++ Js/main.js
@@ -2,6 +2,4 @@ const robotron = document.querySelector(".robo")
 var braco = document.querySelector("#braco");
 var tirar = document.querySelector("#subtrair");
 var adicionar = document.querySelector("#soma");
-
-tirar.addEventListener("click",function(evento){braco.value = parseInt(braco.value) -1;});
 adicionar = addEventListener("click", function(evento){braco.value = parseInt(braco.value) +1;});
