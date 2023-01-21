985e5acd8fe29ca83415551a9fd484a63b09c592
FRANCIELE PASTORI <103603841+FranPastori@users.noreply.github.com>
2023-01-21 17:46:45 -0300

    testar

--- Js/main.js
+++ Js/main.js
@@ -3,5 +3,5 @@ var braco = document.querySelector("#braco");
 var tirar = document.querySelector("#subtrair");
 var adicionar = document.querySelector("#soma");
 
-tirar.addEventListener("click",function(){braco.value = parseInt(braco.value) -1;});
-adicionar = addEventListener("click", function(){braco.value = parseInt(braco.value) +1;});
+tirar.addEventListener("click",function(evento){braco.value = parseInt(braco.value) -1;});
+adicionar = addEventListener("click", function(evento){braco.value = parseInt(braco.value) +1;});
