9c9d50e45c3287aeab685fe9c46bd20b939e820d
FRANCIELE PASTORI <103603841+FranPastori@users.noreply.github.com>
2023-01-21 17:27:10 -0300

    Teste var

--- Js/main.js
+++ Js/main.js
@@ -1,10 +1,13 @@
 const robotron = document.querySelector(".robo")
 var braco = document.querySelector("#braco");
-const tirar = document.querySelector("#subtrair");
-const adicionar = document.querySelector("#adicionar");
+var tirar = document.querySelector("#subtrair");
+var adicionar = document.querySelector("#adicionar");
 
 tirar.addEventListener("click",subtrair);
 adicionar = addEventListener("click", somar)
+
+
+
 function subtrair (){ 
     braco.value =  parseInt(braco.value) -=1 ;
 }
