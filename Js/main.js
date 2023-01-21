2390363afd6846d8b9f61fcce3eaad57addf132f
FRANCIELE PASTORI <103603841+FranPastori@users.noreply.github.com>
2023-01-21 17:24:49 -0300

    ??

--- Js/main.js
+++ Js/main.js
@@ -4,7 +4,7 @@ const tirar = document.querySelector("#subtrair");
 const adicionar = document.querySelector("#adicionar");
 
 tirar.addEventListener("click",subtrair);
-adicionar = addEventListener("click",somar)
+adicionar = addEventListener("click", somar)
 function subtrair (){ 
     braco.value =  parseInt(braco.value) -=1 ;
 }
