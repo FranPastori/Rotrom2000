f909213f219ba6e921ac7e63800d2dd092f0c378
FRANCIELE PASTORI <103603841+FranPastori@users.noreply.github.com>
2023-01-21 17:56:58 -0300

    Melhorando o codigo

--- Js/main.js
+++ Js/main.js
@@ -2,7 +2,19 @@ const robotron = document.querySelector(".robo")
 var braco = document.querySelector("#braco");
 var tirar = document.querySelector("#subtrair");
 var adicionar = document.querySelector("#soma");
+tirar.addEventListener("click", function(evento){manipularDados("tirar")};
+adicionar.addEventListener("click", function(evento){manipularDados("adiconar")};
 
-adicionar.addEventListener("click", function(evento){braco.value = parseInt(braco.value) +1;});
 
-tirar.addEventListener("click", function(evento){braco.value = parseInt(braco.value) -1;});
+
+
+
+function manipularDados (operacao){
+
+    if( operacao===tirar){
+        braco.value = parseInt(braco.value) -1;
+    }
+    else{
+        braco.value = parseInt(braco.value) +1;
+    }
+}
