2e6a472f5547622881075277e8a0d23b81fff37c
FRANCIELE PASTORI <103603841+FranPastori@users.noreply.github.com>
2023-01-21 17:42:27 -0300

    corrigindo bug

--- Js/main.js
+++ Js/main.js
@@ -3,16 +3,5 @@ var braco = document.querySelector("#braco");
 var tirar = document.querySelector("#subtrair");
 var adicionar = document.querySelector("#soma");
 
-tirar.addEventListener("click",remove);
-adicionar = addEventListener("click", somar)
-
-
-function remove (){ 
-
-    braco.value = parseInt(braco.value) -1;
-  }
-
-function somar (){ 
-
-    braco.value = parseInt(braco.value) +1;
-  }
+tirar.addEventListener("click",function(){braco.value = parseInt(braco.value) -1;});
+adicionar = addEventListener("click", function(){braco.value = parseInt(braco.value) +1;});
