1999b7aa644c09bb8cdf362fe50d28bf403fc7c3
FRANCIELE PASTORI <103603841+FranPastori@users.noreply.github.com>
2023-01-21 17:22:05 -0300

    Adicionando braço correção

--- Js/main.js
+++ Js/main.js
@@ -1,9 +1,9 @@
 const robotron = document.querySelector(".robo")
 const braco = document.querySelector("#braco");
-const subtrair = document.querySelector("#subtrair");
+const tirar = document.querySelector("#subtrair");
 const somar = document.querySelector("#adicionar");
 
-subtrair.addEventListener("click",subtrair);
+tirar.addEventListener("click",subtrair);
 somar = addEventListener("click",somar)
 function subtrair (){ 
     braco.value =  parseInt(braco.value) -=1 ;
