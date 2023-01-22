b9af0f1217350d037495e1b23ed5dd28a117e033
FRANCIELE PASTORI <103603841+FranPastori@users.noreply.github.com>
2023-01-22 12:12:30 -0300

    Alterando o html para trablahar com as estastíticas

--- Js/main.js
+++ Js/main.js
@@ -3,7 +3,41 @@ const robotron = document.querySelector(".robo")
 var tirar = document.querySelector("#subtrair");
 var adicionar = document.querySelector("#soma");
 const controle = document.querySelectorAll(".controle-ajuste");
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
 
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
+const estatisitca = querySelectorAll("[data-estatistica]");
+console.log(estatisitca);
 controle.forEach(function(elemento){ elemento.addEventListener("click",(evento) =>{manipularDados(evento.target.textContent,evento.target.parentNode)});});
 
 
